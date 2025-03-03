9e3cea3cfc12944949631050d3a8786e91d705d0
Ubuntu <avenosh@MyVmtop.r45kuqvvtjbeja0jx5lyzvdirc.ax.internal.cloudapp.net>
2025-03-03 13:11:08 +0000

    terraform creating VM

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "cd1d5b6b-777a-429e-b2f5-5cbca419475a"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "Artiom_RG1"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVnet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "myNic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "myNicConfiguration"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "myVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+
+  admin_username      = "adminuser"
+  disable_password_authentication = false
+
+  admin_password      = "Password123!"  # Replace with a secure password
+
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
